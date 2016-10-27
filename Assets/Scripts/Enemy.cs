a36756638a42f1152d433e639c3f66c4c6655e68
hadrianhu888 <hadrian.hu@gmail.com>
2016-10-26 21:36:35 -0400

    upload these files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	GameObject.pathGO; 
+
+	// Use this for initialization
+	void Start () {
+	
+		pathGO = GameObject("Path")
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
